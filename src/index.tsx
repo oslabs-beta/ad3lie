0caa77ac608dbecbcb0f6323fdd6d180691b3dff
Ju Kim <100457090+jukim98@users.noreply.github.com>
2022-05-25 10:26:39 -0700

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
 import {createRoot} from 'react-dom/client'
 import HelloWorld from './components/HelloWorld';
+import { render } from 'react-dom';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<HelloWorld title='YOOOOOO! I got it!!!'/>);
+type title = string;
+
+const root = document.getElementById('root');
+render(
+<HelloWorld title='YOOOOOO! I got it!!!' />, root);
 
 // Doesn't work without correct props
 // root.render(<HelloWorld title={3} />);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+// //   <Provider store={store}>
+//     // <App />
+// //   </Provider>,
+// );
