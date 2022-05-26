645e589fadf595cf584483c70238ae151f18e438
Michael Teifel <11655553+Michael-Teifel@users.noreply.github.com>
2022-05-25 19:53:10 -0700

    Setup Application Layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,11 @@ import { BrowserRouter as Router, Route, Routes, useLocation, HashRouter } from 
 import BarChart from './components/BarChart'
 import ChartCustomizer from './components/ChartCustomizer'
 import NavBar from './components/NavBar'
+import AppLayout from './layouts/AppLayout';
 
 
 export default () => {
   return (
-    <HashRouter>
-      <Router>
-        <div>
-          <NavBar />
-          <ChartCustomizer >
-            <Routes>
-              <Route path="barchart" element={<BarChart />} />
-            </Routes>
-          </ChartCustomizer> 
-        </div>
-      </Router>
-    </HashRouter>
-    
+    <AppLayout />    
   )
 }
