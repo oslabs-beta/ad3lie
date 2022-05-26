f99cb750711fd901cf21be17c83b10c67c9f0cc5
Ju Kim <100457090+jukim98@users.noreply.github.com>
2022-05-25 17:30:54 -0700

    Working on BarChartCustomizer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation, HashRouter } from 'react-router-dom';
 import BarChart from './components/BarChart'
-import ChartCustomizer from './components/BarChartCustomizer'
+import ChartCustomizer from './components/ChartCustomizer'
 import NavBar from './components/NavBar'
 
 
--- src/components/BarChartCustomizer.tsx
+++ src/components/BarChartCustomizer.tsx
@@ -1,36 +1,137 @@
 import React from 'react';
 import { ChangeEvent, useState, Fragment } from 'react';
-import { BarChartContext } from '../ChartContext'
+// import { BarChartContext } from '../ChartContext'
 
-export default function ChartCustomizer () {
+export default function BarChartCustomizer () {
   // type ChartCustomizerProps = {
   //   onChange: (n: number) => void;
   // };
 
-  //import bar chart context from ChartContext or create context here?
-  //use this form to update values (in context)
-  //barchart.tsx will pull updated context 
-
+  // Data: Required object[]
+// Keys: Required string[]
+// xKey: Required string 
+// yKey: Required string
+// xAxisTitle: Optional string default: ‘X-axis’
+// yAxisTitle: Optional string default: ‘Y-axis’
+// Height: Required number
+// Width: Required number
+ 
+  
+  const [data, setData] = useState<object[]>([]);
+  const [keys, setKeys] = useState<string[]>([]);
+  const [xKey, setXKey] = useState('');
+  const [yKey, setYKey] = useState('');
+  const [xAxisTitle, setXAxisTitle] = useState('X-axis');
+  const [yAxisTitle, setYAxisTitle] = useState('Y-axis');
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
   
+
+  const changeData = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setData(getData());
+  }
+
+  const changeKeys = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setKeys(event.target.value);
+  }
+
+  const changeXKey = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setXKey(event.target.value);
+  }
+
+  const changeYKey = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setYKey(event.target.value);
+  }
+
+  const changeXAxisTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setXAxisTitle(event.target.value);
+  }
+
+  const changeYAxisTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setYAxisTitle(event.target.value);
+  }
+
   const changeWidth = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     setWidth(+event.target.value);
   }
+
+  const changeHeight = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setHeight(+event.target.value);
+  }
+
   return (
     <Fragment>
-    <div>Chart Customizer Box</div>
+    <div>Chart Customizer Form</div>
     <section className="form">
         <form onSubmit={() => {}}>
-          <input
+         
+             <input
+            id="set-data"
+            type="array"
+            value={data}
+            onChange={changeData}
+            />
+            <input
+            id="set-keys"
+            type="array"
+            value={keys}
+            onChange={changeKeys}
+            />
+            <input
+            id="set-yKey"
+            type="string"
+            value={yKey}
+            onChange={changeYKey}
+            />
+            <input
+            id="set-xKey"
+            type="string"
+            value={xKey}
+            onChange={changeXKey}
+          />
+            <input
+            id="set-yAxisTitle"
+            type="string"
+            value={yAxisTitle}
+            onChange={changeYAxisTitle}
+          />
+            <input
+            id="set-xAxisTitle"
+            type="string"
+            value={xAxisTitle}
+            onChange={changeXAxisTitle}
+          />
+           <input
             id="set-width"
             type="number"
             value={width}
             onChange={changeWidth}
-          />
+            />
+            <input
+            id="set-height"
+            type="number"
+            value={height}
+            onChange={changeHeight}
+            />
         </form>
       </section>
     </Fragment>
   )
 }
+
+// Data: Required object[]
+// Keys: Required string[]
+// xKey: Required string 
+// yKey: Required string
+// xAxisTitle: Optional string default: ‘X-axis’
+// yAxisTitle: Optional string default: ‘Y-axis’
+// Height: Required number
+// Width: Required number
