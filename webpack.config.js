9c82391141a5acfcaea46b2e99e8254d127bb2aa
Sophia Chiao <37905400+ssdrsv@users.noreply.github.com>
2022-05-26 17:50:17 -0700

    Added webpack config with .jsx
    Co-authored-by: Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,28 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.tsx',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   // devtool: "source-map",
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
   },
   
   module: {
     rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
       {
         test: /.js$/,
         exclude: /node_modules/,
