e16311ed8dbfdb5741f9583c8ed6734a033c2339
Ju Kim <100457090+jukim98@users.noreply.github.com>
2022-06-06 09:58:43 -0400

    Fixed form input to only accept height/width values > 100 to prevent page from crashing. Default set to 500px. Can we add an error dialog window for this?

---
+++ src/utils/handlers.js
@@ -0,0 +1,47 @@
+// Data must be input in JSON format
+export const handleData = (e) => {
+  e.preventDefault();
+  //Input data works for JSON format - see jsonpenguins.txt
+  setData(JSON.parse(e.target.value));
+};
+
+// Data needs to be re-input as key changes, since grouped data is already set in state
+export const handleXKey = (e) => {
+  e.preventDefault();
+  setXKey(e.target.value);
+};
+
+export const handleYKey = (e) => {
+  e.preventDefault();
+  setYKey(e.target.value);
+};
+
+export const handleXAxisLabel = (e) => {
+  e.preventDefault();
+  setXAxisLabel(e.target.value);
+};
+
+export const handleYAxisLabel = (e) => {
+  e.preventDefault();
+  setYAxisLabel(e.target.value);
+};
+
+export const handleWidth = (e) => {
+  e.preventDefault();
+  if (+e.target.value < 100) {
+    console.log('Value must not be less than 100 px. Resetting to default.');
+    setWidth(500);
+    return;
+  }
+  setWidth(+e.target.value);
+};
+
+export const handleHeight = (e) => {
+  e.preventDefault();
+  if (+e.target.value < 100) {
+    console.log('Value must not be less than 100 px. Resetting to default.');
+    setHeight(500);
+    return;
+  }
+  setHeight(+e.target.value);
+};
