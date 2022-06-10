5007a0d50d6f6ca2595d3a18f09fc4ea74067b5a
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2022-06-10 10:02:04 -0700

    need to figure out what .y0 is on line

--- src/components/ChartComponents/JSX/Line.jsx
+++ src/components/ChartComponents/JSX/Line.jsx
@@ -6,7 +6,7 @@ import * as d3 from "d3"
 const Line = ({ data, xAccessor, yAccessor, y0Accessor, width, height, /* type, interpolation, ...props */ }) => {
   const lineGenerator = d3.line()
     .x(xAccessor)
-    .y0(d=> Math.min(0, yscale(yAccessor)))
+    // .y0(d=> Math.min(0, yscale(yAccessor)))
     .y(yAccessor)
     .curve(d3.curveMonotoneX)
 
