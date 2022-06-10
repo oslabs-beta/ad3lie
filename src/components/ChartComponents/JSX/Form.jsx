c32657b57acf0787c3d41a25869b2363f6755134
Ju Kim <100457090+jukim98@users.noreply.github.com>
2022-06-10 10:55:52 -0400

    playing around w tailwind and form dropdowns

--- src/components/ChartComponents/JSX/Form.jsx
+++ src/components/ChartComponents/JSX/Form.jsx
@@ -18,27 +18,50 @@ const Form = ({ properties }) => {
   )
 
   const inputs = properties.map((p,i) => (
-    <div key={i}>
-      <label>
-        {/* `${type-p}` */}
-        {startCase(p)}
-      </label>
-      <textarea
-        id={`set-${p}`}
-        name={`${p}`}
-        placeholder={`${startCase(p)}`}
-        onChange={handleChange}
-        > 
-      </textarea>
-    </div>
+      <div key={i} class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            {/* `${type-p}` */}
+            {startCase(p)}
+          </label>
+          <textarea
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id={`set-${p}`}
+            name={`${p}`}
+            placeholder={`${startCase(p)}`}
+            onChange={handleChange}
+            > 
+          </textarea>
+        </div>
+
+        {/* <div class="w-full md:w-1/2 px-3">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            {startCase(p)}
+          </label>
+          <div class="relative">
+            <select 
+              class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              onChange={handleChange}>
+                {properties.map((p) => {
+                  <option name={p} key={`option-${p}`}>{p}</option>
+                })}
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+            </div>
+          </div>
+        </div> */}
+
+        {/* <p class="text-red-500 text-xs italic">Please fill out this field.</p> */}
+      </div>
     )
   )
 
   return (
     <form class=" w-full max-w-lg" onSubmit={() => {}}>
-      <div class="rounded flex flex-col flex-wrap -mx-3 mb-6">
+      <div class="rounded flex flex-col flex-wrap mx-6">
         {inputs}
-        <h1></h1>
       </div>
     </form>
 
